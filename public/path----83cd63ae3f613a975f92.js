42bb3fe1e1378a991bee4b4cec0b9ed47b4c6388
Charlie Harrington <charlie@whatrocks.org>
2018-06-10 08:40:32 -0700

    fix link in header

--- public/path----83cd63ae3f613a975f92.js
+++ public/path----83cd63ae3f613a975f92.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{109:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Commencement DB"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{108:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Commencement DB"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----83cd63ae3f613a975f92.js.map
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,36 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
+const Header = ({ siteTitle }) => {
+  const prodMode = process.env.NODE_ENV === "production" ? true : false;
+  return (
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        background: "rebeccapurple",
+        marginBottom: "1.45rem"
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <div
+        style={{
+          margin: "0 auto",
+          maxWidth: 960,
+          padding: "1.45rem 1.0875rem"
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to={prodMode ? "/commencement-db" : ""}
+            style={{
+              color: "white",
+              textDecoration: "none"
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
     </div>
-  </div>
-)
+  );
+};
 
-export default Header
+export default Header;
