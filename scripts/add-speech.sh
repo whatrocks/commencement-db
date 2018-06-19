e0a38b29956dfd91bd07622c0ba26e68bc646e10
Charlie Harrington <charlie@whatrocks.org>
2018-06-19 07:15:01 -0700

    update add speech script

--- scripts/add-speech.sh
+++ scripts/add-speech.sh
@@ -5,13 +5,13 @@ echo " "
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
-read -p "speaker: (Ken Burns) " speaker
-speaker=${speaker:-Ken Burns}
+read -p "speaker: (Bruce Wayne) " speaker
+speaker=${speaker:-Bruce Wayne}
 lowercasespeaker=`echo "$speaker" | awk '{print tolower($0)}'`
-read -p "year: (2006) " year
-year=${year:-2006}
-read -p "school: (Georgetown University) " school
-school=${school:-Georgetown University}
+read -p "year: (2018) " year
+year=${year:-2018}
+read -p "school: (Gotham City College) " school
+school=${school:-Gotham City College}
 lowercaseschool=`echo "$school" | awk '{print tolower($0)}'`
 fullPath=`join_by - $year $lowercasespeaker $lowercaseschool`
 read -p "youtube_id: (D1R-jKKp3NA) " youtube_id
@@ -29,24 +29,22 @@ echo "path: $fullPath"
 
 echo " "
 
-if [ "$isMarkdown" == "true" ]; then
 echo "Creating a new speech page based on your input..."
 mkdir "src/pages/${fullPath}"
 echo "---
-path: \"${fullPath}\"
-speaker: \"${speaker}\"
-school: \"${school}\"
+path: /${fullPath}
+speaker: ${speaker}
+school: ${school}
 year: \"${year}\"
-youtube_id: \"${youtube_id}\"
+youtube_id: ${youtube_id}
 ---
 
 Hello, graduates! Get ready for the best speech, ever.
 
     " >> src/pages/${fullPath}/index.md
-fi
 
 echo " "
 echo "Done!"
 echo " "
 echo "Let's start the blog dev server..."
-# yarn develop
+yarn develop
